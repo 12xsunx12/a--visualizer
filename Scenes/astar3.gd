a121424295102568d245acec1a862b46f6a00052
sun <116397424+12xsunx12@users.noreply.github.com>
2024-04-18 02:48:46 -0500

    basic function setup and formatting
    astar runs and prints the starting nodes location, that's it so far.

--- Scenes/astar3.gd
+++ Scenes/astar3.gd
@@ -1,7 +1,26 @@
 extends Node2D
 
 # // - - - - - - - - - - - - - - - - - - - - - - - - - 
-var start_node: Vector2i = Global.start_pos 	# node where A* begins
-var end_node: Vector2i = Global.end_pos 		# the goal node
-var pri_que: Array = [] 						# the priority que
-var path: Array = [] 							# the constructed shortest path
+# How A* Works Via Steps:
+# 1.) pop the start node off the que, and evaluate it's neighbors and add them to the que
+# 2.) evaluate the f value on each node in the que
+# 3.) the lowest f value is popped from the que
+# 4.) popped node is evaluated for neighbor nodes, repeat from step 1
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+var start_node: Nod = Nod.new(Global.start_pos, null) 	# node where A* begins
+var end_node: Nod = Nod.new(Global.end_pos, null) 		# the goal node
+var curr_node: Nod = null 								# popped node being evald
+var pri_que: Array = [start_node] 						# the priority que
+var path: Array = [] 									# the constructed shortest path
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+func _ready():
+	astar()
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+func astar() -> void:
+	curr_node = pri_que.pop_front()
+	print("Current Node: " + str(curr_node.pos))
+	
+	

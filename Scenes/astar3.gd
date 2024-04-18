4a75089b9fe3302f9a4bf64beae92be48746a20a
sun <116397424+12xsunx12@users.noreply.github.com>
2024-04-18 05:33:53 -0500

    will terminate program if a certain max amount of evaluations is exceeded

--- Scenes/astar3.gd
+++ Scenes/astar3.gd
@@ -13,6 +13,7 @@ var end_node: Nod = Nod.new(Global.end_pos, null) 		# the goal node
 var curr_node: Nod = null 								# popped node being evald
 var pri_que: Array = [start_node] 						# the priority que
 var path: Array = [] 									# the constructed shortest path
+var max_evaluations = 500 								# if astar can't find end in 5000 searches, throw error
 
 # // - - - - - - - - - - - - - - - - - - - - - - - - - 
 func _ready():
@@ -22,7 +23,7 @@ func _ready():
 func _get_adj_nodes_simple(node: Nod) -> void:
 	var node_pos = node.pos	
 	var node_right = Nod.new(Vector2i(node_pos.x + 1, node_pos.y), node); node_right.calculate();
-	var node_left = Nod.new(Vector2i(node_pos.x - 1, node_pos.y), node); node_left.calculate();
+	var node_left = Nod.new(Vector2i(node_pos.x - 1, node_pos.y), node); # node_left.calculate();
 	var node_up = Nod.new(Vector2i(node_pos.x, node_pos.y - 1), node); node_up.calculate();
 	var node_down = Nod.new(Vector2i(node_pos.x, node_pos.y + 1), node); node_down.calculate();
 	pri_que.push_front(node_down)
@@ -38,13 +39,12 @@ func _return_lowest_f_in_que() -> int:
 		var i = 0
 		for node in pri_que:
 			i += 1
-			print("Node Pos: \t" + str(node.pos) + " \t Node f: " + str(node.f))
 			if node.f < lowest_f:
 				lowest_f = node.f
 				index = i
 		return index
 	else:
-		print("~ - ~ Error ~ - ~\n_return_lowest_f_in_que: que is empty / null")
+		print("\n\n~ - ~ Error ~ - ~\n_return_lowest_f_in_que: que is empty / null")
 		return 0
 
 # // - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -55,14 +55,18 @@ func _place_lowest_f_at_front() -> void:
 
 # // - - - - - - - - - - - - - - - - - - - - - - - - - 
 func astar() -> void:
+	var counter = 0
 	while pri_que: # for every node in the que
 		curr_node = pri_que.pop_front() # obtain first node in que and remove
 		
 		if curr_node.pos == end_node.pos: # if obtained node is the end node, return
 			pri_que.clear() # clean the array of left over nodes
 			return
+		elif counter >= max_evaluations:
+			print("\n\n~ - ~ Error ~ - ~\n" + "max evals exceeded, could not find end after " + str(max_evaluations) + " searches")
+			break
 		else:
 			curr_node.calculate() # calculate g, h, & f
 			_get_adj_nodes_simple(curr_node) # explore adjacent nodes
 			_place_lowest_f_at_front() # find lowest f node, and place at front of que
-			print(pri_que[0].pos)
+			counter += 1

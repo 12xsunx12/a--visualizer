6d4a9be1fede11f829b9bac8b317cb35f381c7cc
sun <109551210+alstontc@users.noreply.github.com>
2024-04-19 13:04:07 -0500

    made a priority que class

---
+++ Scenes/priQ.gd
@@ -0,0 +1,55 @@
+class_name priQ extends Node2D
+
+var elements: Array = []
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+func _init():
+	elements.resize(0)
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+func enqueue(item):
+	var entry = {"item": item, "priority": item.f}
+	elements.append(entry)
+	var index = elements.size() - 1
+	while index > 0:
+		var parent_index = (index - 1) / 2
+		if elements[parent_index]["priority"] > elements[index]["priority"]:
+			var temp = elements[parent_index]
+			elements[parent_index] = elements[index]
+			elements[index] = temp
+			index = parent_index
+		else:
+			break
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+func dequeue():
+	var min_element = elements[0]["item"]
+	var last_element = elements.pop_back()
+	if elements.size() > 0:
+		var index = 0
+		while true:
+			var left_child = 2 * index + 1
+			var right_child = 2 * index + 2
+			var min_child = -1
+			if left_child < elements.size():
+				min_child = left_child
+			if right_child < elements.size() and elements[right_child]["priority"] < elements[left_child]["priority"]:
+				min_child = right_child
+			if min_child == -1 or last_element["priority"] <= elements[min_child]["priority"]:
+				break
+			elements[index] = elements[min_child]
+			index = min_child
+		elements[index] = last_element
+	return min_element
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+func is_empty() -> bool:
+	return elements.size() == 0
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+func clear() -> void:
+	elements.clear()
+
+# // - - - - - - - - - - - - - - - - - - - - - - - - - 
+func size() -> int:
+	return elements.size()
--- Scenes/test.gd
+++ Scenes/test.gd
@@ -1,24 +1,18 @@
 extends Node2D
 
+var p: priQ = priQ.new()
+var node1: Nod = Nod.new(Vector2i(0,0), null)
+var node2: Nod = Nod.new(Vector2i(5,0), null)
+var node3: Nod = Nod.new(Vector2i(-3,0), null)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	test()
 
 func test():
-	var dic: Dictionary = {}
-	var cord0: Vector2i = Vector2i(5,5)
-	var cord1: Vector2i = Vector2i(10,10)
-	var cord2: Vector2i = Vector2i(15,15)
-	var node0: Nod = Nod.new(cord0, null)
-	var node1: Nod = Nod.new(cord1, null)
-	var node2: Nod = Nod.new(cord2, null)
-	
-	dic[cord0] = node0
-	dic[cord1] = node1
-	dic[cord2] = node2
-	
-	for key in dic:
-		print(dic[key].pos)
+	p.enqueue(node1)
+	p.enqueue(node2)
+	p.enqueue(node3)
+	print(p.dequeue().pos)
 	
 	
